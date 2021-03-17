4ca5e426491ae0f5d887a716e6bb3a40b6b03196
Nagesh0921 <62686884+Nagesh0921@users.noreply.github.com>
2021-03-17 13:54:00 +0530

    Create Uglynumbers.java

---
+++ Uglynumbers.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static UglyNumbers u = new UglyNumbers();
+    public int nthUglyNumber(int n) {
+        return u.nums[n-1];
+    }
+}
+
+class UglyNumbers{
+    public int[] nums = new int[1690];
+    UglyNumbers(){
+        nums[0] = 1;
+        int ugly, i2 = 0, i3 = 0, i5 = 0;
+        for(int i=1; i<1690; i++){
+            ugly = Math.min(Math.min(nums[i2]*2, nums[i3]*3), nums[i5]*5);
+            nums[i] = ugly;
+            if(ugly == nums[i2]*2){
+                i2++;
+            } 
+            if(ugly == nums[i3]*3){
+                i3++;
+            }
+            if(ugly == nums[i5]*5){
+                i5++;
+            }
+        }
+    }
+}
+
+//Time Complexity is O(1)...as we are precomputing it. Space Complexity O(1)
