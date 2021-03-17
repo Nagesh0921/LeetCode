ec9dc3c08ccbb48c9872779b0de3de58d1446fb0
Nagesh0921 <62686884+Nagesh0921@users.noreply.github.com>
2021-03-17 11:04:15 +0530

    Create MergeTwoSortedList.java

---
+++ Google/MergeTwoSortedList.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode tempHead = new ListNode(-1);
+        ListNode prev = tempHead;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = (l1 == null) ? l2 : l1;
+        return tempHead.next;
+    }
+}
+
+// Time Complexity O(n+m) iterating both lists, Space Complexity O(1)
+
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Two Sorted Lists.
+//Memory Usage: 38.4 MB, less than 64.61% of Java online submissions for Merge Two Sorted Lists.
+
+// Recurssion
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        if(l1.val <= l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+//Time Complexity O(n+m) Space Complexity O(n+m) - recurssive stack
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Two Sorted Lists.
+//Memory Usage: 38.2 MB, less than 77.23% of Java online submissions for Merge Two Sorted Lists.
