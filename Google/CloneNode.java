360776bdcdff863c96a3dc87e86e6f532ff2bc8b
Nagesh0921 <62686884+Nagesh0921@users.noreply.github.com>
2021-03-20 19:52:17 +0530

    Create CloneNode.java

---
+++ Google/CloneNode.java
@@ -0,0 +1,90 @@
+//DFS
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    HashMap<Node, Node> visited = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node == null){
+            return node;
+        }
+        
+        if(visited.containsKey(node)){
+            return visited.get(node);
+        }
+        
+        Node cloneNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, cloneNode);
+        
+        for(Node neighbor : node.neighbors){
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+        
+        return cloneNode;
+    }
+}
+
+//Time Complexity O(N+M) Space Complexity O(N)
+
+//BFS
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null){
+            return node;
+        }
+        
+        HashMap<Node, Node> visited = new HashMap<>();
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(node);
+        visited.put(node, new Node(node.val, new ArrayList<>()));
+        
+        while(!queue.isEmpty()){
+            Node n = queue.pop();
+            for(Node neighbor : n.neighbors){
+                if(!visited.containsKey(neighbor)){
+                    visited.put(neighbor, new Node(neighbor.val, new ArrayList<>()));
+                    queue.add(neighbor);
+                }
+                visited.get(n).neighbors.add(visited.get(neighbor));
+            }
+        }
+        return visited.get(node);
+    }
+}
