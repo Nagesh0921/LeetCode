bce493c0a416e9b714f7169fb40d117e3042c554
Nagesh0921 <62686884+Nagesh0921@users.noreply.github.com>
2022-01-29 10:00:57 +0530

    Update PeakElement.java

--- Google/PeakElement.java
+++ Google/PeakElement.java
@@ -1,22 +1,24 @@
-class Solution
-{
-	// Function to find the peak element
-	// arr[]: input array
-	// n: size of array a[]
-	public int peakElement(int[] arr,int n)
-    {
-       //add code here.
-       int left = 0, right = arr.length-1;
-       while(left < right){
-           int mid = (left + right)/2;
-           if(arr[mid] > arr[mid+1]){
-               right = mid;
-           } else {
-               left = mid+1;
-           }
-       }
-       return left;
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int peak = nums[0], peakIndex = 0;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] > peak){
+                peak = nums[i];
+                peakIndex = i;
+            }
+        }
+        return peakIndex;
+        // int left = 0, right = nums.length-1;
+        // while(left < right){
+        //     int mid = (left + right)/2;
+        //     if(nums[mid] > nums[mid+1]){
+        //         right = mid;
+        //     } else {
+        //         left = mid+1;
+        //     }
+        // }
+        // return left;
     }
 }
 
-//Time Complexity O(log N)
+//Time Complexity is O(log n)
